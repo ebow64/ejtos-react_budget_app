7ff8e15d1286c9bde8422f34466c3af4bda1c975
ebow64 <77249372+ebow64@users.noreply.github.com>
2024-04-02 21:07:05 -0700

    Updated to add Decrease by 10 column

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
+
 const ExpenseList = () => {
     const { expenses } = useContext(AppContext);
     return (
@@ -10,6 +11,7 @@ const ExpenseList = () => {
               <th scope="col">Department</th>
               <th scope="col">Allocated Budget</th>
               <th scope="col">Increase by 10</th>
+              <th scope="col">Decrease by 10</th>
               <th scope="col">Delete</th>
             </tr>
           </thead>
